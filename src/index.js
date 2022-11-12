3a418df03fb04b7536c84bd70d485689a93ff37d
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-12 13:36:41 -0600

    Update index.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const path = require("node:path");
+
+const { result } = require("./utils/dotenv");
+const { ShardingManager } = require("discord.js");
+
+const client = path.join(__dirname, "client", "client.js");
+const manager = new ShardingManager(client, {
+  token: result.parsed.TOKEN
+});
+
+manager.on("shardCreate", (shard) => {
+  console.log(`Launched Shard ${shard.id}!`);
+});
+
+manager.spawn();
