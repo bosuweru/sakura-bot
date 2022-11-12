084560539b37959fb9416742f72b49da0fbbb9af
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-12 13:36:46 -0600

    Add client.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/client/client.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const eventsPath = path.join(__dirname, "..", "events");
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
+
+client.login(process.env.TOKEN);
