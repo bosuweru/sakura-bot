9c108beb2026636d02ef46ee290b8f4b5368a283
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-12 13:40:58 -0600

    [Feature] Dotenv (#1)
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/utils/dotenv.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const path = require("node:path");
+
+const dotenv = require("dotenv");
+const plugin = require("dotenv-expand");
+
+const object = dotenv.config({
+  path: path.join(__dirname, "..", "..", "private", ".env"),
+  debug: false,
+  encoding: "utf8",
+  override: false
+});
+
+const result = plugin.expand(object);
+
+module.exports = { result }
