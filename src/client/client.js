62a9d07bd7452f0c0ccefc763ff44831356aae90
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-16 02:42:04 -0600

    Update client.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/client/client.js
+++ src/client/client.js
@@ -3,9 +3,23 @@
 const fs = require("node:fs");
 const path = require("node:path");
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "..", "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  client.commands.set(command.data.name, command);
+}
 
 const eventsPath = path.join(__dirname, "..", "events");
 const eventFiles = fs
