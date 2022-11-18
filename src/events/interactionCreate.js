9cb86047721e842b24a4a344c32cb2cd37e1b531
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-18 16:16:18 -0600

    [Release] v0.1.0 (rerelease) (#9)
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,29 @@
+/**
+ * @file Initializes the 'interactionCreate' event.
+ * @author bosuweru <116328571+bosuweru@users.noreply.github.com>
+ * @license AGPL-3.0
+ * @version 0.1.0
+ */
+
+"use strict";
+
+const { Log } = require("../utils/winston");
+const log = new Log();
+
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.InteractionCreate,
+  once: false,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      log.write("error", `Cannot execute interaction.`);
+    }
+  },
+};
