f532d1ab19df4eb53589f7e6b824b7f3d752b240
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-18 16:10:57 -0600

    Update ping.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,10 @@
+/**
+ * @file Initializes the 'ping' command.
+ * @author bosuweru <116328571+bosuweru@users.noreply.github.com>
+ * @license AGPL-3.0
+ * @version 0.1.0
+ */
+
 "use strict";
 
 const { SlashCommandBuilder } = require("discord.js");
@@ -5,8 +12,16 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with pong!"),
+    .setDescription("Checks latency."),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    const sent = await interaction.reply({
+      content: "Pinging...",
+      fetchReply: true,
+    });
+    interaction.editReply(
+      `Roundtrip Latency: ${
+        sent.createdTimestamp - interaction.createdTimestamp
+      }ms`
+    );
   },
 };
