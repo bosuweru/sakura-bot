0b29eb56e6497d9ab87751319e6858edf47cdb76
Allan <116328571+bosuweru@users.noreply.github.com>
2022-12-12 01:51:01 -0600

    Update retroAchievements.js
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- src/helpers/retroAchievements.js
+++ src/helpers/retroAchievements.js
@@ -16,11 +16,54 @@ const key = process.env.RETROACHIEVEMENTS_KEY;
 const mode = "json";
 const user = process.env.RETROACHIEVEMENTS_USER;
 
+function getGameList(platform, collection) {
+  const gameList = [];
+  const gameData = collection.get(platform);
+
+  gameData.forEach((item) => {
+    gameList.push(item.Title);
+  });
+
+  return gameList;
+}
+
+function getConsoleList(collection) {
+  return Array.from(collection.keys());
+}
+
+async function fetchGameData(id) {
+  const url = `https://ra.hfc-essentials.com/game_list.php?user=${user}&key=${key}&console=${id}&mode=${mode}`;
+  const res = await axios.get(url);
+
+  return res.data;
+}
+
 async function fetchConsoleData() {
   const url = `https://ra.hfc-essentials.com/console_id.php?user=${user}&key=${key}&mode=${mode}`;
   const res = await axios.get(url);
 
   return res.data;
+}
+
+async function writeGameData(id, platform) {
+  let fileName;
+
+  switch (platform) {
+    case "PC-8000/8800":
+      fileName = "pc-8000";
+      break;
+    default:
+      fileName = platform.toLowerCase();
+      break;
+  }
+
+  // eslint-disable-next-line prettier/prettier
+  const file = path.join(__dirname, "..", "..", "private", "data", "GameData", `${fileName}.json`);
+  const data = await fetchGameData(id);
+
+  const result = JSON.stringify(data);
+
+  fs.writeFileSync(file, result);
 }
 
 async function writeConsoleData() {
@@ -31,6 +74,24 @@ async function writeConsoleData() {
   const result = JSON.stringify(data);
 
   fs.writeFileSync(file, result);
+}
+
+function cacheGameData(collection) {
+  // eslint-disable-next-line prettier/prettier
+  const gameFolder = path.join(__dirname, "..", "..", "private", "data", "GameData");
+  const gameFiles = fs
+    .readdirSync(gameFolder)
+    .filter((file) => file.endsWith(".json"));
+
+  for (const gameFile of gameFiles) {
+    const file = path.join(gameFolder, gameFile);
+    const data = fs.readFileSync(file);
+
+    const parsed = JSON.parse(data);
+    const result = parsed.game[0];
+
+    if (result[0]) collection.set(result[0].ConsoleName, result);
+  }
 }
 
 function cacheConsoleData(collection) {
@@ -46,4 +107,13 @@ function cacheConsoleData(collection) {
   });
 }
 
-module.exports = { cacheConsoleData, writeConsoleData };
+module.exports = {
+  getGameList,
+  cacheGameData,
+  fetchGameData,
+  writeGameData,
+  getConsoleList,
+  cacheConsoleData,
+  fetchConsoleData,
+  writeConsoleData,
+};
